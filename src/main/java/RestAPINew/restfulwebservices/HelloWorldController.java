5879b02a97b5e04559e7b14d2ed45b2e5699c4d9
MANIKRISHNA SANGANABATLA <msanganabatla@csu.fullerton.edu>
2023-10-24 15:27:38 -0700

    Basic HelloWorld RestAPI created

--- src/main/java/RestAPINew/restfulwebservices/HelloWorldController.java
+++ src/main/java/RestAPINew/restfulwebservices/HelloWorldController.java
@@ -1,8 +1,6 @@
 package RestAPINew.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //This will make this controller a Rest controller
