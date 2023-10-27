9ad47f1b8a519d58906c40ca5e9fc86903e6b0c8
MANIKRISHNA SANGANABATLA <msanganabatla@csu.fullerton.edu>
2023-10-26 17:09:01 -0700

    Implementing Exception Handling - 404 Resource Not Found

--- src/main/java/RestAPINew/restfulwebservices/user/UserResource.java
+++ src/main/java/RestAPINew/restfulwebservices/user/UserResource.java
@@ -21,8 +21,12 @@ public class UserResource {
 
     }
     @GetMapping("/users/{id}")
-    public  User retrieveAllUser(@PathVariable int id){
-        return service.findOne(id);
+    public  User retrieveUser(@PathVariable int id){
+        User user= service.findOne(id);
+        if(user==null){
+            throw new UserNotFoundException("id : "+id);
+        }
+        return user;
     }
     //POST/users
     @PostMapping("/users")
